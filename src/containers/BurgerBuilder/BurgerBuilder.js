bc864ee69e4bd5e88b89939294bc45cdd28726dc
Soroush81@gmail.com <soroush81@gmail.com>
2021-04-10 17:04:45 +0430

    first changes

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Axiliary from '../components/La'
+
+const BurgerBuilder = (props) => (
+    <Auxiliary>
+        <div>
+            Burger
+        </div>
+        <div>
+            Build Controls
+        </div>
+    </Auxiliary>
+);
+
+export default BurgerBuilder;
